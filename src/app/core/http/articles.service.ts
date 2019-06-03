b5fc2d722156641b542f9e7d161c661224f319ed
BLOT Martial <martial.blot@outlook.fr>
2019-06-03 11:22:01 +0200

    Begin create get article by ID

--- src/app/core/http/articles.service.ts
+++ src/app/core/http/articles.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticlesService {
 
-  constructor() { }
+  public service: HttpClient;
+
+  constructor(public http: HttpClient) { }
+
+  public getArticle(id) {
+    const  obs: Observable<any> = this.http
+    .get(`${environment.apiUrl}/articles/${id}`);
+    const  treatment  = (response:any) => {
+      return  response as Booking[];
+    };
+    return  obs.pipe(map(treatment));
+  }
 }
