5dfd733755a8802f993127ff01189c4e845e965c
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-06-03 11:21:28 +0200

    GetAllArticles service + Articles Model

--- src/app/core/http/articles.service.ts
+++ src/app/core/http/articles.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Articles } from "src/app/shared/models/articles.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ArticlesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public api = `${environment.apiUrl}`;
+
+  public getArticles(): Observable<Articles[]> {
+    return this.http.get(`${this.api}/articles`).pipe(
+      map((allArticles: any) => {
+        return allArticles as Articles[];
+      })
+    );
+  }
 }
