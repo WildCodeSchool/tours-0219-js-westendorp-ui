2755d3c3f5e61fcdee7d45f5cc9156395ecbe0c7
BLOT Martial <martial.blot@outlook.fr>
2019-06-06 11:42:29 +0200

    Début composant login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginModalComponent } from './features/login-modal/login-modal.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
    { path: 'login', component: LoginModalComponent },
+   { path: '', component: HomeComponent },
 ];
 
 @NgModule({
--- src/app/features/login-modal/login-modal.component.ts
+++ src/app/features/login-modal/login-modal.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login-modal',
   templateUrl: './login-modal.component.html',
   styleUrls: ['./login-modal.component.scss'],
+  providers: [NgbModalConfig, NgbModal],
 })
 export class LoginModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+
+  open(content) {
+    this.modalService.open(content);
+  }
 
   ngOnInit() {
   }
