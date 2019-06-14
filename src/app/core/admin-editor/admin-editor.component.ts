7ab7a4d02fb7cf25befe9654de24759662737515
Jérémy Foucault <jeremfouc@orange.fr>
2019-06-14 09:35:53 +0200

    modif route aprés click sur modifier

--- src/app/core/admin-editor/admin-editor.component.ts
+++ src/app/core/admin-editor/admin-editor.component.ts
@@ -3,7 +3,7 @@ import { EditorService } from '../services/editor.service';
 import { Article } from 'src/app/shared/models/article.model';
 import { ArticlesService } from '../http/articles.service';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-admin-editor',
   templateUrl: './admin-editor.component.html',
@@ -17,20 +17,27 @@ export class AdminEditorComponent implements OnInit {
   constructor(
     private editorService: EditorService,
     private articlesService: ArticlesService,
+    private _location: Location,
   ) { }
 
   ngOnInit() {
     this.article = this.editorService.article;
     this.type = this.editorService.typeOfContent;
-  }
+    }
 
   updateArticle(id: string, content: Article) {
     this.articlesService.updateArticle(id, content).subscribe((newArticle: Article) => {
       this.article = newArticle;
       console.log(this.article);
+      this.backClick();
     });
   }
   deleteArticle(id: string) {
     this.articlesService.deleteArticle(id);
   }
+
+  backClick() {
+    this._location.back();
+  }
+
 }
