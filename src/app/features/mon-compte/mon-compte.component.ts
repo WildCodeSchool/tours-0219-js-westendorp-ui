ff1900c057d4a6e5da23db2bc81870c0aeeee7a9
BLOT Martial <martial.blot@outlook.fr>
2019-06-19 13:31:27 +0200

    Formulaires compte création ok mise en place des methode à suivre

--- src/app/features/mon-compte/mon-compte.component.ts
+++ src/app/features/mon-compte/mon-compte.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-mon-compte',
   templateUrl: './mon-compte.component.html',
-  styleUrls: ['./mon-compte.component.scss']
+  styleUrls: ['./mon-compte.component.scss'],
 })
 export class MonCompteComponent implements OnInit {
+  hiddenFormMdp = false;
+  nextFormMdp = true;
+  submitFormMdp = true;
+  hiddenFormId = false;
+  showFormId = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  nextMdp() {
+    this.hiddenFormMdp = true;
+    this.nextFormMdp = false;
+  }
+
+  validMdp() {
+    this.nextFormMdp = true;
+    this.submitFormMdp = false;
+  }
+
+  registerMdp() {
+
+  }
+
+  nextId() {
+    this.hiddenFormId = true;
+    this.showFormId = false;
+  }
+
+  registerID() {
+
+  }
 }
