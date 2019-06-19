dc9af834911829da855c5b1756a7a6e7d76eb12f
BLOT Martial <martial.blot@outlook.fr>
2019-06-19 09:23:39 +0200

    composant timeline créé

---
+++ src/app/features/timeline/timeline.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticlesService } from 'src/app/core/http/articles.service';
+import { Article } from 'src/app/shared/models/article.model';
+import { LoginService } from 'src/app/core/services/login.service';
+import { EditorService } from 'src/app/core/services/editor.service';
+
+@Component({
+  selector: 'app-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.scss'],
+})
+export class TimelineComponent implements OnInit {
+
+  isLogin = !this.service.isLogin();
+  newArticle: Article = new Article('', 'nouveau titre', 'nouveau texte', undefined ,  '', '',  '');
+  articlesList: Article[] = [];
+
+  constructor(
+    private service: LoginService,
+    private editorService: EditorService,
+    private articlesService: ArticlesService,
+  ) { }
+
+  ngOnInit() {
+    this.articlesService.getArticles().subscribe((articles) => {
+      for (let i = 0; i < articles.length; i += 1) {
+        if (articles[i].section === 'presentation') {
+          this.articlesList.push(articles[i]);
+        }
+      }
+    });
+  }
+
+  createActivity() {
+    this.editorService.article = this.newArticle;
+    this.editorService.typeOfContent = 'title';
+    this.editorService.typeEdition = true;
+  }
+
+  deleteCard(id) {
+    this.articlesList.splice(this.articlesList.findIndex(a => a.id === id), 1);
+  }
+
+}
