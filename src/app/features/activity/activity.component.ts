e9fe56fca33809c78c5cf62f1858a76d5f1b3d3a
BLOT Martial <martial.blot@outlook.fr>
2019-06-19 09:26:11 +0200

    Fix lint

--- src/app/features/activity/activity.component.ts
+++ src/app/features/activity/activity.component.ts
@@ -12,7 +12,7 @@ import { ArticlesService } from 'src/app/core/http/articles.service';
 export class ActivityComponent implements OnInit {
 
   isLogin = !this.service.isLogin();
-  newArticle: Article = new Article('', "Titre de l'article", "Contenu de l'article", undefined ,  '', '',  '');
+  newArticle: Article = new Article('', 'Titre de l\'article', 'Contenu de l\'article', undefined ,  '', '',  '');
   articlesList: Article[] = [];
 
   constructor(
