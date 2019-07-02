0687eac93e178e3cfb52a7ade2f8be8674b235a4
felix <felixok@live.fr>
2019-07-02 11:38:48 +0200

    animation route / footer vh css

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from './animation';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  animations: [fadeAnimation]
 })
 export class HeaderComponent implements OnInit {
 
-  isShown = false;
 
   constructor() { }
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
+  }
 
   ngOnInit() {
   }
