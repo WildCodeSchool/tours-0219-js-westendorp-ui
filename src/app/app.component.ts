18195f92bd75e88c72fdb595b5bb0ffeebf3d6c3
felix <felixok@live.fr>
2019-07-03 16:34:36 +0200

    Scroll top to on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-""
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'westendorp-ui';
+  constructor(private router: Router) { }
 
-  
+  ngOnInit() {
+
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
+
 }
